7bec5151de840ffec721ce7766ac2b4a9123c969
razzrohith <razzrohith@gmail.com>
2026-05-05 22:16:52 -0500

    Update runtime-config.js with correct project ID

--- assets/js/runtime-config.js
+++ assets/js/runtime-config.js
@@ -1,1 +1,4 @@
-﻿// Placeholder for runtime config
+window.__RAITHUFRESH_CONFIG__ = {
+  "SUPABASE_URL": "https://tdkoxwyxcnneqbuolzgm.supabase.co",
+  "SUPABASE_ANON_KEY": "sb_publishable_vW7W4f11mD8uFTcBMTMQig_XkQk7_0t"
+};

4f0f93331353f317ef92360cd3846c8d61e4354a
razzrohith <razzrohith@gmail.com>
2026-05-07 18:55:07 -0500

    Deploy reservation flow trust polish

--- assets/js/runtime-config.js
+++ assets/js/runtime-config.js
@@ -1,4 +1,1 @@
-window.__RAITHUFRESH_CONFIG__ = {
-  "SUPABASE_URL": "https://tdkoxwyxcnneqbuolzgm.supabase.co",
-  "SUPABASE_ANON_KEY": "sb_publishable_vW7W4f11mD8uFTcBMTMQig_XkQk7_0t"
-};
+﻿// Placeholder for runtime config
